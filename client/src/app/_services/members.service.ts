cb5f6cbcdab2bfbeb1fed4ba6f716528125ca178
Fakhri Albari <fakhri.albari@ecomindo.com>
2022-12-15 18:40:01 +0700

    Updating Resource FE

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -8,13 +10,32 @@ import { Member } from '../_models/member';
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
+
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'user');
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'user').pipe(
+      map(members => {
+        this.members = members;
+        return this.members;
+      })
+    );
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.username == username);
+    if (member) return of(member)
     return this.http.get<Member>(this.baseUrl + 'user/' + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'user', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = {...this.members[index], ...member}
+      })
+    );
   }
 }
